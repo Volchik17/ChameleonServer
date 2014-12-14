473bca771d002db7601bad0b5f4c59f2460e20a8
volchik17 <volchik_@mtu-net.ru>
2014-12-14 10:03:20 +0300

    start

---
+++ src/com/bssys/chameleon/servlet/ChameleonServlet.java
@@ -0,0 +1,16 @@
+package com.bssys.chameleon.servlet;
+
+import java.io.IOException;
+
+/**
+ * Created by volchik on 14.12.14.
+ */
+public class ChameleonServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
